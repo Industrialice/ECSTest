d7040d72c7e31d8bb36dfa4887fb674d5b7ddeaa
Industrialice <iindustrialice@gmail.com>
2019-04-22 16:28:09 +0300

    Template code refactoring

--- ECS/SystemHelpers.hpp
+++ ECS/SystemHelpers.hpp
@@ -7,50 +7,49 @@ namespace ECSTest
         return std::tie(left.isWriteAccess, left.requirement, left.type) < std::tie(right.isWriteAccess, right.requirement, right.type);
     }
 
-	template <typename T> struct _GetComponentType
-	{
-		using type = T;
-	};
-
-	template <typename T> struct _GetComponentType<SubtractiveComponent<T>>
-	{
-		using type = T;
-	};
-
-	template <typename T> struct _GetComponentType<Array<T>>
-	{
-		using type = T;
-	};
-
-	template <typename T> struct _GetComponentType<Array<SubtractiveComponent<T>>>
-	{
-		//static_assert(false, "Subtractive component cannot be inside an array");
-		using type = T;
-	};
-
-	template <typename T> constexpr bool _IsArray(Array<T>) { return true; }
-	template <typename T> constexpr bool _IsArray(T) { return false; }
-
-    // used by direct systems to convert void **array into proper function argument types
-    template <auto Method, typename types = typename FunctionInfo::Info<decltype(Method)>::args, uiw count = std::tuple_size_v<types>> struct _AcceptCaller
+    struct _SystemHelperFuncs
     {
+        template <typename T> struct GetComponentType
+        {
+            using type = T;
+        };
+
+        template <typename T> struct GetComponentType<SubtractiveComponent<T>>
+        {
+            using type = T;
+        };
+
+        template <typename T> struct GetComponentType<Array<T>>
+        {
+            using type = T;
+        };
+
+        template <typename T> struct GetComponentType<Array<SubtractiveComponent<T>>>
+        {
+            //static_assert(false, "Subtractive component cannot be inside an array");
+            using type = T;
+        };
+
+        template <typename T> static constexpr bool IsArray(Array<T>) { return true; }
+        template <typename T> static constexpr bool IsArray(T) { return false; }
+
         // converts void * into a properly typed T argument - a pointer or a reference
-        template <typename types, uiw index> static FORCEINLINE auto Convert(void **args) -> decltype(auto)
+        template <typename types, uiw index> static FORCEINLINE auto ConvertArgument(void **args) -> decltype(auto)
         {
             using T = std::tuple_element_t<index, types>;
             void *arg = args[index];
 
-			using pureType = std::remove_cv_t<std::remove_pointer_t<std::remove_reference_t<T>>>;
+            using pureType = std::remove_cv_t<std::remove_pointer_t<std::remove_reference_t<T>>>;
 
-			static constexpr bool isRefOrPtr = std::is_reference_v<T> || std::is_pointer_v<T>;
-			static constexpr bool isSubtractive = std::is_base_of_v<_SubtractiveComponentBase, pureType>;
-			static constexpr bool isArray = _IsArray(pureType());
+            static constexpr bool isRefOrPtr = std::is_reference_v<T> || std::is_pointer_v<T>;
+            static constexpr bool isSubtractive = std::is_base_of_v<_SubtractiveComponentBase, pureType>;
+            static constexpr bool isArray = IsArray(pureType());
             static_assert(isSubtractive || isRefOrPtr, "Type must be either reference or pointer");
-			static_assert(!isSubtractive || !isRefOrPtr, "Subtractive component cannot be passed by reference or pointer");
-			static_assert(isSubtractive || isArray, "Non-subtractive components must be passed using Array<T>");
-			static_assert(!isSubtractive || !isArray, "Subtractive component cannot be inside an array");
+            static_assert(!isSubtractive || !isRefOrPtr, "Subtractive component cannot be passed by reference or pointer");
+            static_assert(isSubtractive || isArray, "Non-subtractive components must be passed using Array<T>");
+            static_assert(!isSubtractive || !isArray, "Subtractive component cannot be inside an array");
 
-			using componentType = typename _GetComponentType<pureType>::type;
+            using componentType = typename GetComponentType<pureType>::type;
 
             if constexpr (std::is_reference_v<T>)
             {
@@ -64,84 +63,86 @@ namespace ECSTest
                 static_assert(!std::is_pointer_v<bare> && !std::is_reference_v<bare>, "Type cannot be pointer to reference/pointer");
                 return (T)arg;
             }
-			else
-			{
-				ASSUME(arg == nullptr);
-				return nullptr;
-			}
+            else
+            {
+                ASSUME(arg == nullptr);
+                return nullptr;
+            }
         }
 
-        template <typename T, uiw... Indexes> static FORCEINLINE void Call(T *object, System::Environment &env, void **array, std::index_sequence<Indexes...>)
+        // used by direct systems to convert void **array into proper function argument types
+        template <auto Method, typename types, typename T, uiw... Indexes> static FORCEINLINE void CallAccept(T *object, System::Environment &env, void **array, std::index_sequence<Indexes...>)
         {
-        	(object->*Method)(env, Convert<types, Indexes>(array)...);
+            (object->*Method)(env, ConvertArgument<types, Indexes>(array)...);
+        }
+
+        // converts argument type (like Array<Component> &) into System::RequestedComponent
+        template <typename T> static constexpr System::RequestedComponent ArgumentToComponent()
+        {
+            using TPure = std::remove_pointer_t<std::remove_reference_t<T>>;
+            using componentType = typename GetComponentType<std::remove_cv_t<TPure>>::type;
+            constexpr RequirementForComponent availability =
+                std::is_reference_v<T> ?
+                RequirementForComponent::Required :
+                (std::is_pointer_v<T> ?
+                    RequirementForComponent::Optional :
+                    (std::is_base_of_v<_SubtractiveComponentBase, T> ?
+                        RequirementForComponent::Subtractive :
+                        (RequirementForComponent)i8_max));
+            static_assert((i32)availability != i8_max);
+            return {componentType::GetTypeId(), !std::is_const_v<TPure>, availability};
+        }
+
+        // TODO: check for types that appear more than once (which is an error)
+        template <typename T, uiw... Indexes> static constexpr std::array<System::RequestedComponent, sizeof...(Indexes)> TupleToComponentsArray(std::index_sequence<Indexes...>)
+        {
+            return {ArgumentToComponent<std::tuple_element_t<Indexes, T>>()...};
+        }
+
+        template <uiw size>[[nodiscard]] static constexpr pair<std::array<System::RequestedComponent, size>, uiw> FindMatchingComponents(const std::array<System::RequestedComponent, size> &arr, RequirementForComponent requirement)
+        {
+            std::array<System::RequestedComponent, size> components{};
+            uiw target = 0;
+            for (uiw source = 0; source < arr.size(); ++source)
+            {
+                if (arr[source].requirement == requirement)
+                {
+                    components[target++] = arr[source];
+                }
+            }
+            return {components, target};
+        }
+
+        template <uiw size>[[nodiscard]] static constexpr pair<std::array<System::RequestedComponent, size>, uiw> FindComponentsWithWriteAccess(const std::array<System::RequestedComponent, size> &arr)
+        {
+            std::array<System::RequestedComponent, size> components{};
+            uiw target = 0;
+            for (uiw source = 0; source < arr.size(); ++source)
+            {
+                if (arr[source].requirement != RequirementForComponent::Subtractive && arr[source].isWriteAccess)
+                {
+                    components[target++] = arr[source];
+                }
+            }
+            return {components, target};
+        }
+
+        template <uiw size>[[nodiscard]] static constexpr pair<std::array<pair<StableTypeId, RequirementForComponent>, size>, uiw> FindArchetypeDefiningComponents(const std::array<System::RequestedComponent, size> &arr)
+        {
+            std::array<pair<StableTypeId, RequirementForComponent>, size> components{};
+            uiw target = 0;
+            for (uiw source = 0; source < arr.size(); ++source)
+            {
+                if (arr[source].requirement == RequirementForComponent::Required || arr[source].requirement == RequirementForComponent::Subtractive)
+                {
+                    components[target].first = arr[source].type;
+                    components[target].second = arr[source].requirement;
+                    ++target;
+                }
+            }
+            return {components, target};
         }
     };
-
-    // converts argument type (like Array<Component> &) into System::RequestedComponent
-    template <typename T> constexpr System::RequestedComponent _ArgumentToComponent()
-    {
-        using TPure = std::remove_pointer_t<std::remove_reference_t<T>>;
-		using componentType = typename _GetComponentType<std::remove_cv_t<TPure>>::type;
-		constexpr RequirementForComponent availability =
-			std::is_reference_v<T> ?
-			RequirementForComponent::Required :
-			(std::is_pointer_v<T> ?
-				RequirementForComponent::Optional :
-				(std::is_base_of_v<_SubtractiveComponentBase, T> ?
-					RequirementForComponent::Subtractive :
-					(RequirementForComponent)i8_max));
-		static_assert((i32)availability != i8_max);
-		return {componentType::GetTypeId(), !std::is_const_v<TPure>, availability};
-    }
-
-    template <typename T, uiw... Indexes> constexpr std::array<System::RequestedComponent, sizeof...(Indexes)> _TupleToComponentsArray(std::index_sequence<Indexes...>)
-    {
-        return {_ArgumentToComponent<std::tuple_element_t<Indexes, T>>()...};
-    }
-
-    template <uiw size> [[nodiscard]] constexpr pair<std::array<System::RequestedComponent, size>, uiw> _FindMatchingComponents(const std::array<System::RequestedComponent, size> &arr, RequirementForComponent requirement)
-    {
-        std::array<System::RequestedComponent, size> components{};
-        uiw target = 0;
-        for (uiw source = 0; source < arr.size(); ++source)
-        {
-            if (arr[source].requirement == requirement)
-            {
-                components[target++] = arr[source];
-            }
-        }
-        return {components, target};
-    }
-
-    template <uiw size> [[nodiscard]] constexpr pair<std::array<System::RequestedComponent, size>, uiw> _FindComponentsWithWriteAccess(const std::array<System::RequestedComponent, size> &arr)
-    {
-        std::array<System::RequestedComponent, size> components{};
-        uiw target = 0;
-        for (uiw source = 0; source < arr.size(); ++source)
-        {
-            if (arr[source].requirement != RequirementForComponent::Subtractive && arr[source].isWriteAccess)
-            {
-                components[target++] = arr[source];
-            }
-        }
-        return {components, target};
-    }
-
-    template <uiw size> [[nodiscard]] constexpr pair<std::array<pair<StableTypeId, RequirementForComponent>, size>, uiw> _FindArchetypeDefiningComponents(const std::array<System::RequestedComponent, size> &arr)
-    {
-        std::array<pair<StableTypeId, RequirementForComponent>, size> components{};
-        uiw target = 0;
-        for (uiw source = 0; source < arr.size(); ++source)
-        {
-            if (arr[source].requirement == RequirementForComponent::Required || arr[source].requirement == RequirementForComponent::Subtractive)
-            {
-                components[target].first = arr[source].type;
-                components[target].second = arr[source].requirement;
-                ++target;
-            }
-        }
-        return {components, target};
-    }
 }
 
 // TODO: optimize it so std::arrays use only as much space as they actually need
@@ -152,13 +153,13 @@ namespace ECSTest
         using thisType = std::remove_reference_t<std::remove_cv_t<decltype(*this)>>; \
 		struct Local { static void Temp(__VA_ARGS__); }; \
 		using types = typename FunctionInfo::Info<decltype(Local::Temp)>::args; \
-        static constexpr auto arr = _TupleToComponentsArray<types>(std::make_index_sequence<std::tuple_size_v<types>>()); \
+        static constexpr auto arr = _SystemHelperFuncs::TupleToComponentsArray<types>(std::make_index_sequence<std::tuple_size_v<types>>()); \
         static constexpr auto arrSorted = Funcs::SortCompileTime(arr); \
-        static constexpr auto required = _FindMatchingComponents(arrSorted, RequirementForComponent::Required); \
-        static constexpr auto opt = _FindMatchingComponents(arrSorted, RequirementForComponent::Optional); \
-        static constexpr auto subtractive = _FindMatchingComponents(arrSorted, RequirementForComponent::Subtractive); \
-        static constexpr auto writeAccess = _FindComponentsWithWriteAccess(arrSorted); \
-        static constexpr auto archetypeDefining = _FindArchetypeDefiningComponents(arrSorted); \
+        static constexpr auto required = _SystemHelperFuncs::FindMatchingComponents(arrSorted, RequirementForComponent::Required); \
+        static constexpr auto opt = _SystemHelperFuncs::FindMatchingComponents(arrSorted, RequirementForComponent::Optional); \
+        static constexpr auto subtractive = _SystemHelperFuncs::FindMatchingComponents(arrSorted, RequirementForComponent::Subtractive); \
+        static constexpr auto writeAccess = _SystemHelperFuncs::FindComponentsWithWriteAccess(arrSorted); \
+        static constexpr auto archetypeDefining = _SystemHelperFuncs::FindArchetypeDefiningComponents(arrSorted); \
         static constexpr Requests requests = \
         { \
             ToArray(required.first.data(), required.second), \
@@ -167,7 +168,8 @@ namespace ECSTest
             ToArray(writeAccess.first.data(), writeAccess.second), \
             ToArray(archetypeDefining.first.data(), archetypeDefining.second), \
             ToArray(arrSorted), \
-            ToArray(arr) \
+            ToArray(arr), \
+            nullopt \
         }; \
         return requests; \
     } \
@@ -178,7 +180,7 @@ namespace ECSTest
 		struct Local { static void Temp(__VA_ARGS__); }; \
 		using types = typename FunctionInfo::Info<decltype(Local::Temp)>::args; \
 		static constexpr uiw count = std::tuple_size_v<types>; \
-        _AcceptCaller<&thisType::Update, types>::Call(this, env, array, std::make_index_sequence<count>()); \
+        _SystemHelperFuncs::CallAccept<&thisType::Update, types>(this, env, array, std::make_index_sequence<count>()); \
     } \
     void Update(Environment &env, __VA_ARGS__)
 
@@ -188,13 +190,13 @@ namespace ECSTest
         using thisType = std::remove_reference_t<std::remove_cv_t<decltype(*this)>>; \
 		struct Local { static void Temp(__VA_ARGS__); }; \
 		using types = typename FunctionInfo::Info<decltype(Local::Temp)>::args; \
-        static constexpr auto arr = _TupleToComponentsArray<types>(std::make_index_sequence<std::tuple_size_v<types>>()); \
+        static constexpr auto arr = _SystemHelperFuncs::TupleToComponentsArray<types>(std::make_index_sequence<std::tuple_size_v<types>>()); \
         static constexpr auto arrSorted = Funcs::SortCompileTime(arr); \
-        static constexpr auto required = _FindMatchingComponents(arrSorted, RequirementForComponent::Required); \
-        static constexpr auto opt = _FindMatchingComponents(arrSorted, RequirementForComponent::Optional); \
-        static constexpr auto subtractive = _FindMatchingComponents(arrSorted, RequirementForComponent::Subtractive); \
-        static constexpr auto writeAccess = _FindComponentsWithWriteAccess(arrSorted); \
-        static constexpr auto archetypeDefining = _FindArchetypeDefiningComponents(arrSorted); \
+        static constexpr auto required = _SystemHelperFuncs::FindMatchingComponents(arrSorted, RequirementForComponent::Required); \
+        static constexpr auto opt = _SystemHelperFuncs::FindMatchingComponents(arrSorted, RequirementForComponent::Optional); \
+        static constexpr auto subtractive = _SystemHelperFuncs::FindMatchingComponents(arrSorted, RequirementForComponent::Subtractive); \
+        static constexpr auto writeAccess = _SystemHelperFuncs::FindComponentsWithWriteAccess(arrSorted); \
+        static constexpr auto archetypeDefining = _SystemHelperFuncs::FindArchetypeDefiningComponents(arrSorted); \
         static constexpr Requests requests = \
         { \
             ToArray(required.first.data(), required.second), \
