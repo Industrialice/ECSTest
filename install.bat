5802f1c8b2906f0b208d49b97d2b28be535b5dbb
Industrialice <iindustrialice@gmail.com>
2019-08-23 19:24:36 +0300

    Added creating symbolic links for the Assets folder as well

--- install.bat
+++ install.bat
@@ -13,6 +13,7 @@ mklink /H "Debug_x86/PhysXCooking_32.dll" "External\PhysX\x86\debug\PhysXCooking
 mklink /H "Debug_x86/PhysXDevice.dll" "External\PhysX\x86\debug\PhysXDevice.dll"
 mklink /H "Debug_x86/PhysXFoundation_32.dll" "External\PhysX\x86\debug\PhysXFoundation_32.dll"
 mklink /H "Debug_x86/PhysXGpu_32.dll" "External\PhysX\x86\debug\PhysXGpu_32.dll"
+mklink /J "Debug_x86/Assets" "ECSEngine\Assets"
 
 mklink /H "Debug_x64/assimp-vc142-mtd.dll" "External\Assimp\x64\debug\assimp-vc142-mtd.dll"
 mklink /H "Debug_x64/zlibd.dll" "External\Assimp\x64\debug\zlibd.dll"
@@ -22,6 +23,7 @@ mklink /H "Debug_x64/PhysXCooking_64.dll" "External\PhysX\x64\debug\PhysXCooking
 mklink /H "Debug_x64/PhysXDevice64.dll" "External\PhysX\x64\debug\PhysXDevice64.dll"
 mklink /H "Debug_x64/PhysXFoundation_64.dll" "External\PhysX\x64\debug\PhysXFoundation_64.dll"
 mklink /H "Debug_x64/PhysXGpu_64.dll" "External\PhysX\x64\debug\PhysXGpu_64.dll"
+mklink /J "Debug_x64/Assets" "ECSEngine\Assets"
 
 mklink /H "Release_x86/assimp-vc142-mt.dll" "External\Assimp\x86\release\assimp-vc142-mt.dll"
 mklink /H "Release_x86/zlib.dll" "External\Assimp\x86\release\zlib.dll"
@@ -31,6 +33,7 @@ mklink /H "Release_x86/PhysXCooking_32.dll" "External\PhysX\x86\release\PhysXCoo
 mklink /H "Release_x86/PhysXDevice.dll" "External\PhysX\x86\release\PhysXDevice.dll"
 mklink /H "Release_x86/PhysXFoundation_32.dll" "External\PhysX\x86\release\PhysXFoundation_32.dll"
 mklink /H "Release_x86/PhysXGpu_32.dll" "External\PhysX\x86\release\PhysXGpu_32.dll"
+mklink /J "Release_x86/Assets" "ECSEngine\Assets"
 
 mklink /H "Release_x64/assimp-vc142-mt.dll" "External\Assimp\x64\release\assimp-vc142-mt.dll"
 mklink /H "Release_x64/zlib.dll" "External\Assimp\x64\release\zlib.dll"
@@ -40,3 +43,4 @@ mklink /H "Release_x64/PhysXCooking_64.dll" "External\PhysX\x64\release\PhysXCoo
 mklink /H "Release_x64/PhysXDevice64.dll" "External\PhysX\x64\release\PhysXDevice64.dll"
 mklink /H "Release_x64/PhysXFoundation_64.dll" "External\PhysX\x64\release\PhysXFoundation_64.dll"
 mklink /H "Release_x64/PhysXGpu_64.dll" "External\PhysX\x64\release\PhysXGpu_64.dll"
+mklink /J "Release_x64/Assets" "ECSEngine\Assets"
